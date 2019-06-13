4b6851b15a734a7dfac3e87c1976465d9e88013d
Jai Dev <35428869+Jai-kishan@users.noreply.github.com>
2019-06-13 14:07:03 +0530

    Update merge_sort.py

--- practice/merge_sort.py
+++ practice/merge_sort.py
@@ -1,4 +1,5 @@
 def merge(left,right):
+	import ipdb ; ipdb.set_trace()  #### ipdb
 	result = []
 	i,j = 0, 0
 	while i<len(left) and j< len(right):
@@ -18,6 +19,7 @@ def mergesort(lst): # pass the argument of lis [5,3,4,2]
 	if(len(lst) <= 1): # []
 		return lst
 	mid = int(len(lst)/2)
+	import ipdb ; ipdb.set_trace() # ipdb
 	left = mergesort(lst[:mid])
 	right = mergesort(lst[mid:])
 	return merge(left,right)
